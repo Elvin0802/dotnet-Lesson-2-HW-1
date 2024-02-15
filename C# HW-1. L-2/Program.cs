ae228fa6003f0841d163fdea70bc970924790f03
Elvin0802 <elvincode1517@gmail.com>
2024-02-15 22:09:20 +0400

    Update 2.0

--- C# HW-1. L-2/Program.cs
+++ C# HW-1. L-2/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using System.Security.Cryptography;
 
-byte option = 1;
+sbyte option = 1;
 
 
 while (true)
@@ -11,11 +11,11 @@ while (true)
     #region First Menu 
     if (option == 1) Console.ForegroundColor = ConsoleColor.Yellow;
     else Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("Start Calculating");
+    Console.WriteLine("\n\t\tStart Calculating");
 
     if (option == 0) Console.ForegroundColor = ConsoleColor.Yellow;
     else Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("Exit");
+    Console.WriteLine("\n\t\tExit");
     #endregion
 
     // Get KB Key
@@ -45,70 +45,76 @@ while (true)
     {
         if (option == 1)
         {
-            byte option2 = 1;
+            sbyte option2 = 0;
 
             while (true)
             {
                 Console.Clear();
 
                 #region Second Menu 
+
+                if (option2 == 0) Console.ForegroundColor = ConsoleColor.Magenta;
+                else Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\n\t\tBack To Menu.");
+
                 if (option2 == 1) Console.ForegroundColor = ConsoleColor.Magenta;
                 else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nOperator:  n1 + n2");
+                Console.WriteLine("\n\t\tOperator:  n1 + n2");
 
-                if (option2 == 2) Console.ForegroundColor = ConsoleColor.Yellow;
+                if (option2 == 2) Console.ForegroundColor = ConsoleColor.Magenta;
                 else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nOperator:  n1 - n2");
+                Console.WriteLine("\n\t\tOperator:  n1 - n2");
 
-                if (option2 == 3) Console.ForegroundColor = ConsoleColor.Yellow;
+                if (option2 == 3) Console.ForegroundColor = ConsoleColor.Magenta;
                 else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nOperator:  n1 * n2");
+                Console.WriteLine("\n\t\tOperator:  n1 * n2");
 
-                if (option2 == 4) Console.ForegroundColor = ConsoleColor.Yellow;
+                if (option2 == 4) Console.ForegroundColor = ConsoleColor.Magenta;
                 else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nOperator:  n1 / n2");
+                Console.WriteLine("\n\t\tOperator:  n1 / n2");
 
-                if (option2 == 5) Console.ForegroundColor = ConsoleColor.Yellow;
+                if (option2 == 5) Console.ForegroundColor = ConsoleColor.Magenta;
                 else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nOperator:  n1 ^ n2");
+                Console.WriteLine("\n\t\tOperator:  n1 ^ n2");
 
-                if (option2 == 0) Console.ForegroundColor = ConsoleColor.Yellow;
-                else Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\t\nBack To Menu.");
                 #endregion
 
                 // Get KB Key
                 ConsoleKeyInfo c2_key = Console.ReadKey(true);
 
                 #region Check Second KB Key 
+
                 if (c2_key.Key == ConsoleKey.UpArrow || c2_key.Key == ConsoleKey.W)
                 {
-                    option2++; if (option2 > 5) option2 = 0;
+                    option2--; if (option2 < 0) option2 = 5;
                 }
                 else if (c2_key.Key == ConsoleKey.DownArrow || c2_key.Key == ConsoleKey.S)
                 {
-                    option2--; if (option2 < 0) option2 = 5;
+                    option2++; if (option2 > 5) option2 = 0;
                 }
                 else if (c2_key.Key == ConsoleKey.RightArrow || c2_key.Key == ConsoleKey.D)
                 {
-                    option2++; if (option2 > 5) option2 = 0;
+                    option2--; if (option2 < 0) option2 = 5;
                 }
                 else if (c2_key.Key == ConsoleKey.LeftArrow || c2_key.Key == ConsoleKey.A)
                 {
-                    option2--; if (option2 < 0) option2 = 5;
+                    option2++; if (option2 > 5) option2 = 0;
                 }
+
                 #endregion
 
                 #region Check Second Enter Key 
+
                 if (c2_key.Key == ConsoleKey.Enter)
                 {
+                    Console.Write("\n\n\n");
                     double num1 = 0.0, num2 = 0.0, result = 0.0;
 
                     if (option2 == 1)
                     {
-                        Console.WriteLine("\nEnter the 1st number : ");
+                        Console.WriteLine("\n\tEnter the 1st number : ");
                         double.TryParse(Console.ReadLine(), out num1);
-                        Console.WriteLine("\nEnter the 2nd number : ");
+                        Console.WriteLine("\n\tEnter the 2nd number : ");
                         double.TryParse(Console.ReadLine(), out num2);
 
                         result = num1+num2;
@@ -117,9 +123,9 @@ while (true)
                     }
                     else if (option2 == 2)
                     {
-                        Console.WriteLine("\nEnter the 1st number : ");
+                        Console.WriteLine("\n\tEnter the 1st number : ");
                         double.TryParse(Console.ReadLine(), out num1);
-                        Console.WriteLine("\nEnter the 2nd number : ");
+                        Console.WriteLine("\n\tEnter the 2nd number : ");
                         double.TryParse(Console.ReadLine(), out num2);
 
                         result = num1-num2;
@@ -128,9 +134,9 @@ while (true)
                     }
                     else if (option2 == 3)
                     {
-                        Console.WriteLine("\nEnter the 1st number : ");
+                        Console.WriteLine("\n\tEnter the 1st number : ");
                         double.TryParse(Console.ReadLine(), out num1);
-                        Console.WriteLine("\nEnter the 2nd number : ");
+                        Console.WriteLine("\n\tEnter the 2nd number : ");
                         double.TryParse(Console.ReadLine(), out num2);
 
                         result = num1*num2;
@@ -139,9 +145,9 @@ while (true)
                     }
                     else if (option2 == 4)
                     {
-                        Console.WriteLine("\nEnter the 1st number : ");
+                        Console.WriteLine("\n\tEnter the 1st number : ");
                         double.TryParse(Console.ReadLine(), out num1);
-                        Console.WriteLine("\nEnter the 2nd number : ");
+                        Console.WriteLine("\n\tEnter the 2nd number : ");
                         double.TryParse(Console.ReadLine(), out num2);
 
                         if (num1 == 0.0 || num2 == 0.0)
@@ -150,7 +156,7 @@ while (true)
                             Console.WriteLine("Can not Divide by Zero !!!");
                             Thread.Sleep(1200);
                             Console.ForegroundColor = ConsoleColor.White;
-                            break;
+                            continue;
                         }
 
                         result = num1/num2;
@@ -159,7 +165,17 @@ while (true)
                     }
                     else if (option2 == 5)
                     {
-                        // for i < n2 ; result *= n1
+                        Console.WriteLine("\n\tEnter the 1st number : ");
+                        double.TryParse(Console.ReadLine(), out num1);
+                        Console.WriteLine("\n\tEnter the 2nd number : ");
+                        double.TryParse(Console.ReadLine(), out num2);
+
+                        result = 1.0;
+
+                        for (double i = 0.0; i < num2; i++)
+                            result*= num1;
+
+                        Console.WriteLine($"\n{num1} ^ {num2} = {result} ..");
                     }
                     else if (option2 == 0)
                     {
@@ -167,16 +183,16 @@ while (true)
                         break;
                     }
                 }
-                else if (option == 0)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("\n\t\tBYE BYE !\n\n");
-                    Thread.Sleep(1500);
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                }
                 #endregion
             }
+        }
+        else if (option == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\t\tBYE BYE !\n\n");
+            Thread.Sleep(1500);
+            Console.ForegroundColor = ConsoleColor.Red;
+            break;
         }
     }
     #endregion
